bcfc4eaa781bb5349c8fad35f8e0c6e515fa53d4
fkoh111 <frederik_kok@icloud.com>
2020-02-16 11:10:57 +0100

    added more tests for extract folder fun

--- R/extract_folder_info.R
+++ R/extract_folder_info.R
@@ -14,8 +14,14 @@
 #' }
 
 extract_folder_info <- function(path, attribute) {
+  
+  if(missing(path)) {
+    stop("You forgot to specify a path")
+  }
+  
+  if(missing(attribute)) {
+    stop("You forgot to specify an attribute")
+  }
+  
   file.info(dir(path, full.names=TRUE))[,attribute][file.info(dir(path, full.names=TRUE))$isdir]
 }
-
-
-
