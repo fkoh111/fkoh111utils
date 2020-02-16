2a55c5f5aa6d3ceb9d88be5a7373be44de3628e3
fkoh111 <frederik_kok@icloud.com>
2020-02-16 11:25:46 +0100

    refined test path in tests

--- tests/testthat/test-extract_folder_info.R
+++ tests/testthat/test-extract_folder_info.R
@@ -4,21 +4,21 @@ test_that("we're receiving appropriate error messages", {
   testthat::expect_error(fkoh111::extract_folder_info(),
                          "You forgot to specify a path", fixed = TRUE)
   
-  testthat::expect_error(fkoh111::extract_folder_info(path = getwd()),
+  testthat::expect_error(fkoh111::extract_folder_info(path = path_to_src),
                          "You forgot to specify an attribute", fixed = TRUE)
   }
 )
 
 test_that("we're getting back expected formats", {
   
-  testthat::expect_is(fkoh111::extract_folder_info(path = getwd(), "size"), "numeric")
-  testthat::expect_is(fkoh111::extract_folder_info(path = getwd(), "uname"), "character")
-  testthat::expect_is(fkoh111::extract_folder_info(path = getwd(), "uid"), "integer")
-  testthat::expect_is(fkoh111::extract_folder_info(path = getwd(), "grname"), "character")
-  testthat::expect_is(fkoh111::extract_folder_info(path = getwd(), "gid"), "integer")
-  testthat::expect_is(fkoh111::extract_folder_info(path = getwd(), "ctime"), "POSIXct")
-  testthat::expect_is(fkoh111::extract_folder_info(path = getwd(), "mtime"), "POSIXct")
-  testthat::expect_is(fkoh111::extract_folder_info(path = getwd(), "atime"), "POSIXct")
+  testthat::expect_is(fkoh111::extract_folder_info(path = path_to_src, "size"), "numeric")
+  testthat::expect_is(fkoh111::extract_folder_info(path = path_to_src, "uname"), "character")
+  testthat::expect_is(fkoh111::extract_folder_info(path = path_to_src, "uid"), "integer")
+  testthat::expect_is(fkoh111::extract_folder_info(path = path_to_src, "grname"), "character")
+  testthat::expect_is(fkoh111::extract_folder_info(path = path_to_src, "gid"), "integer")
+  testthat::expect_is(fkoh111::extract_folder_info(path = path_to_src, "ctime"), "POSIXct")
+  testthat::expect_is(fkoh111::extract_folder_info(path = path_to_src, "mtime"), "POSIXct")
+  testthat::expect_is(fkoh111::extract_folder_info(path = path_to_src, "atime"), "POSIXct")
   
   }
 )
